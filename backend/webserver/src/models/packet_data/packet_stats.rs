2346a2c8e81d3b1b844dfe1e88efe8794ad03c0d
mbrowley24 <michael.a.browley@outlook.com>
2025-04-13 21:17:29 -0600

    packet stats struct created new function created

--- backend/webserver/src/models/packet_data/packet_stats.rs
+++ backend/webserver/src/models/packet_data/packet_stats.rs
@@ -31,9 +31,35 @@ pub struct PacketStatus {
     pub mac_addr_count        : usize,
     pub mac_add_top_talkers   : HashMap<MacAddr, usize>,
     pub packet_count          : usize,
-    pub packets_retransmitted : usize,
+    pub retransmissions_count : usize,
+    pub retransmissions       : HashMap<IpAddr, usize>,
     pub top_talkers           : HashMap<IpAddr, usize>,
     pub top_conservations     : HashMap<FlowKey, usize>,
+    pub total_protocols       : HashMap<IpNextHeaderProtocol, usize>,
+    pub top_ports             : HashMap<u16, usize>,
     pub total_bytes           : usize,
 
+}
+
+
+impl PacketStatus {
+    pub fn new() -> Self {
+
+        Self{
+            arp                   : HashMap::new(),
+            bytes_per_protocol    : HashMap::new(),
+            ipv4                  : HashMap::new(),
+            ipv6                  : HashMap::new(),
+            mac_addr_count        : 0,
+            mac_add_top_talkers   : HashMap::new(),
+            packet_count          : 0,
+            retransmissions_count : 0,
+            retransmissions       : HashMap::new(),
+            top_talkers           : HashMap::new(),
+            top_conservations     : HashMap::new(),
+            total_protocols       : HashMap::new(),
+            top_ports             : HashMap::new(),
+            total_bytes           : 0,
+        }
+    }
 }
